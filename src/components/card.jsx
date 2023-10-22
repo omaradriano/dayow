d267fd788b638448781eca8a0b3f06f80ea16d0d
Omar Adrian <oadrian38@gmail.com>
2023-10-21 22:34:57 -0600

    Modificación en el componente card para poder renderizar datos reales de manera dinámica

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,28 +1,64 @@
+import { useState, useEffect } from 'react';
 import Button from './Button.jsx'
 
 import Icon from './Icon.jsx';
+const apikey = 'd12a4203bb8343b6b70235323232110'
 
 const Card = () => {
 
-    return (
-        <section className="card">
-            <Icon icon='light_mode' customIconClassName='weatherIcon weatherIcon__sunny'></Icon>
-            <div className='weatherInfo'>
-                <p className="weatherInfo__state">Sunny</p>
-                <p className="weatherInfo__grades">22°</p>
-                <p className="weatherInfo__location">Chihuahua,Chihuahua</p>
-            </div>
-            <div className="weatherStadistics">
-                <div className="weatherStadistics__left">
-                    <p>Wind</p>
-                    <p>Humidity</p>
+    const [isLoading, setIsLoading] = useState(false)
+    const [weather, setWeather] = useState({})
+    useEffect(() => {
+        fetch(`http://api.weatherapi.com/v1/current.json?key=${apikey}&q=chihuahua`)
+            .then(res =>{
+                return res.json()
+            })
+            .then(data => {
+                let weatherData = {
+                    location: `${data.location.name}, ${data.location.region}`,
+                    temp: data.current.temp_c,
+                    wind: data.current.wind_kph,
+                    humidity: data.current.humidity,
+                    condition: data.current.condition.text,
+                    wind_direction: data.current.wind_dir
+                }
+                // console.log(weatherData)
+                setWeather(weatherData)
+                setIsLoading(false) //Variable usada para saber si los datos están cargados o no
+            })
+    }, []);
+
+    if (!isLoading) {
+        return (
+            <section className="card">
+                <Icon icon='light_mode' customIconClassName='weatherIcon weatherIcon__sunny'></Icon>
+                <div className='weatherInfo'>
+                    <p className="weatherInfo__state">{weather.condition}</p>
+                    <p className="weatherInfo__grades">{weather.temp}°</p>
+                    <p className="weatherInfo__location">{weather.location}</p>
                 </div>
-                <div className="weatherStadistics__right">
-                    <p>N 8 MPH</p>
-                    <p>52%</p>
+                <div className="weatherStadistics">
+                    <div className="weatherStadistics__left">
+                        <p>Viento</p>
+                        <p>Humedad</p>
+                    </div>
+                    <div className="weatherStadistics__right">
+                        <p>{weather.wind_direction} {weather.wind} KPH</p>
+                        <p>{weather.humidity}%</p>
+                    </div>
                 </div>
-            </div>
-        </section>
-    )
+                <Button></Button>
+            </section>
+        )
+    } else {
+        return(
+            <section className="card">
+                <Icon icon='light_mode' customIconClassName='weatherIcon weatherIcon__sunny'></Icon>
+                <div>Loading</div>
+            </section>
+        )
+    }
+
+
 }
 export default Card
